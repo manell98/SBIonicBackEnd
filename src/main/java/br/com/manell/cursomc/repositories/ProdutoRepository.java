5f0d57bf05ea98a7dbdbaccb8f573054b2f1a7bf
Emanuell Santos <dfmanu06@gmail.com>
2019-06-11 00:07:26 -0300

    Produto e associacao many to many

---
+++ src/main/java/br/com/manell/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package br.com.manell.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.manell.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
