67357b862fb945e8da5439aaaa8bcb7c156b2a0e
Emanuell Santos <dfmanu06@gmail.com>
2019-06-22 01:19:31 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/br/com/manell/cursomc/services/PedidoService.java
+++ src/main/java/br/com/manell/cursomc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.com.manell.cursomc.domain.Cliente;
 import br.com.manell.cursomc.domain.ItemPedido;
 import br.com.manell.cursomc.domain.PagamentoComBoleto;
 import br.com.manell.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import br.com.manell.cursomc.domain.enums.EstadoPagamento;
 import br.com.manell.cursomc.repositories.ItemPedidoRepository;
 import br.com.manell.cursomc.repositories.PagamentoRepository;
 import br.com.manell.cursomc.repositories.PedidoRepository;
+import br.com.manell.cursomc.security.UserSS;
+import br.com.manell.cursomc.services.exceptions.AuthorizationException;
 import br.com.manell.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -62,6 +68,16 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 
 }
