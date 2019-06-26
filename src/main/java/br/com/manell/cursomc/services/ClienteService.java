d8b934505590e72ff9a270bb2bb85de86d2e711a
Emanuell Santos <dfmanu06@gmail.com>
2019-06-25 23:46:46 -0300

    Enviando imagem via endpoint

--- src/main/java/br/com/manell/cursomc/services/ClienteService.java
+++ src/main/java/br/com/manell/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package br.com.manell.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import br.com.manell.cursomc.domain.Cidade;
 import br.com.manell.cursomc.domain.Cliente;
@@ -36,6 +38,9 @@ public class ClienteService {
 	
 	@Autowired
 	private BCryptPasswordEncoder pe;
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public Cliente find(Integer id){
 		
@@ -105,6 +110,10 @@ public class ClienteService {
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 
 }
